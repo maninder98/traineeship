b267f7c90ec757d79c522e72728bc1c22db4d49b
Maninder Singh <maninder.maninder.singh@hva.nl>
2021-03-30 13:43:54 +0200

    Hoofdstuk 14 collections gemaakt

--- src/main/java/h13/House.java
+++ src/main/java/h13/House.java
@@ -4,14 +4,14 @@ import h10.Human;
 
 public class House<T extends Human> {
 
-    private T owner;
+    private T houseOwner;
 
     public House(T owner) {
-        this.owner = owner;
+        this.houseOwner = owner;
     }
 
     @Override
     public String toString() {
-        return "This house is owned by [" + owner + "] and it says [" + owner.greet() + "].";
+        return "This house is owned by [" + houseOwner + "] and it says [" + houseOwner.greet() + "].";
     }
 }
--- src/test/java/h11/PersonTest.java
+++ src/test/java/h11/PersonTest.java
@@ -3,6 +3,10 @@ package h11;
 import h10.Human;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTest {
@@ -20,10 +24,24 @@ class PersonTest {
         Human human = p1.createSubHuman();
         String greet = human.greet();
         System.out.println(greet);
-
-
     }
 
+    // Lab 14 Testing hascode tset
+
+    @Test
+    void testHashcodes() {
+        HashSet<Person> persons = new HashSet<>();
+
+        persons.add(new Person("Piet", 23));
+        persons.add(new Person("Piet", 23));
+        persons.add(new Person("Piet", 23));
+        persons.add(new Person("Jan", 32));
+        persons.add(new Person("Jan", 32));
+
+        System.out.println(persons.size());
+        assertTrue(persons.size() == 2);
+
+    }
 
 }
 
